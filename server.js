354adeb6497ec780a1c15c6a6b8b1bee03da7871
jeremyorion <jeremyorion@icloud.com>
2019-10-16 19:51:24 -0500

    updating DB

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-solid-34521',
-        user: '',
-        password: '',
-        database: 'smartbrain',
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 })
 
